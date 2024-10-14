ed208d3c7ad3b82e90140b4776c7249a53ba0b27
wishalta <rolandas.lebed@gmail.com>
2024-10-14 15:05:45 +0300

    8 done, for naudojau

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from collections import Counter
+from inspect import istraceback
+from operator import index
 
 # 1
 
@@ -30,6 +33,66 @@
 #     lst.append(ele)
 #
 # print(lst)
+# sk = len(lst)
+# print(f'{sk} Pazymiu kiekis')
 
 # 4
+
+# list = ['Vilnius', 'Kaunas', 'Klaipeda', 'Panevezys']
+# print(list)
+# add = input('Miestas: ')
+# a = int(input('Vieta: '))   #int jei nori ivesti skaiciu (input kai zodis)
+# print(a)
+# list.insert(a, add)
+# print(list)
+# # a = print('miesto vieta: ')
+# #
+# # list.insert[1, 2]
+# # print(list)
+
+# 5
+
+# list = [0, 1, 2, 3, 4, 5, 6, 7]
+# print('Yra 7 duomenys')
+# list.pop()
+# a = int(input('Kiek norit istrinti: '))
+# for x in range(a):
+#     list. pop()
+# print(list)
+
+# 6
+
+# list = [1, 2, 3, 4, 5, 6, 7]
+# g = len(list)
+# print(g)
+# if len(list) > 5:
+#     list.clear()
+#     print(list)
+# else:
+#     print(True)
+
+# 7
+
+# list = ['opa', 'gola', 'hola', 'goal', 'meaning']
+# element = input('Jusu norimas zodis: ')
+# g = list.index(element)
+# print(f'Positiot: {g}')
+# if element in list:
+#     print(True)
+
+# 8
+
+list = []
+n = int(input('Kiek pazymiu jus turite: '))
+for i in range (0, n):
+    variable = int(input('Grade: '))
+    list.append(variable)
+print(list)
+
+y = 10
+
+x =[i for i in list if i==y]
+print(len(x))
+
+# 9
 
