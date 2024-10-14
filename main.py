b0d5c94dd2bb735be4850d0db8717eaa88fce973
wishalta <rolandas.lebed@gmail.com>
2024-10-14 11:09:16 +0300

    pirmi 3

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 
-list = [Rolandas, Lukas, Staniulis]
+# 1
 
-print(list)
+# list = ["Rolandas", "Lukas", "Staniulis"]
+#
+# print(list)
+# print(list[0])
+# print(list[2])
+# print(len(list))
 
-print('Labas')
+# 2
+
+# high = [182, 170, 200]
+# print(high)
+# print(len(high))
+
+# 3
+
+
+# # creating an empty list
+# lst = []
+#
+# # number of elements as input
+# n = int(input("Enter number of elements : "))
+#
+# # iterating till the range
+# for i in range(0, n):
+#     ele = int(input())
+#     # adding the element
+#     lst.append(ele)
+#
+# print(lst)
+
+# 4
+
